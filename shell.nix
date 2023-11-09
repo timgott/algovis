0a820b81d500569e5d184a39e8c3b036b330d524
Tim Göttlicher <timg00@outlook.de>
2023-11-09 16:32:15 +0100

    Move package.json to toplevel

--- shell.nix
+++ shell.nix
@@ -4,5 +4,5 @@ let
   nodePkgs = pkgs.nodePackages;
 in
 pkgs.mkShell {
-  packages = [ nodePkgs.typescript nodePkgs.typescript-language-server ];
+  packages = [ pkgs.nodejs_20 nodePkgs.typescript-language-server ];
 }
