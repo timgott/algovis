7186035a1bcb9071f07cfc7ca97defa83c49a3a3
Tim Göttlicher <timg00@outlook.de>
2023-11-09 15:14:23 +0100

    Add lsp and top level tsconfig

--- shell.nix
+++ shell.nix
@@ -1,5 +1,8 @@
 # NixOS 23.05
 { pkgs ? import <nixpkgs> {} }:
+let
+  nodePkgs = pkgs.nodePackages;
+in
 pkgs.mkShell {
-  packages = [ pkgs.nodePackages.typescript ];
+  packages = [ nodePkgs.typescript nodePkgs.typescript-language-server ];
 }
