1fc06ec0d027d01f4adcfe64141b1014c6fa50a5
Tim Göttlicher <timg00@outlook.de>
2026-06-24 14:51:37 +0300

    update shell.nix to nixpkgs 26.05

--- shell.nix
+++ shell.nix
@@ -1,8 +1,5 @@
 # NixOS 23.05
 { pkgs ? import <nixpkgs> {} }:
-let
-  nodePkgs = pkgs.nodePackages;
-in
 pkgs.mkShell {
-  packages = [ pkgs.nodejs_24 nodePkgs.typescript-language-server pkgs.deno ];
+  packages = [ pkgs.nodejs_24 pkgs.typescript-language-server pkgs.deno ];
 }
